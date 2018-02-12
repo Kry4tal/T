0af6084ba470ae74a85e813b58fc88b691890922
Domoyo <taeyond@outlook.com>
2018-02-13 00:46:57 +0800

    Create T.sh

---
+++ T.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+seven_seven="seven is a goodboy"
+sakura="seven is a goodboy"
+l="祝他幸福"
+echo ${seven_seven
+if [ $seven_seven=sakura ]
+then
+    echo ${l}
+fi
