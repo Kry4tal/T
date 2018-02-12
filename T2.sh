6e341f0d3abbda9eb0fb0f027368951d1ca54ce7
Domoyo <taeyond@outlook.com>
2018-02-13 01:10:18 +0800

    Update T2.sh

--- T2.sh
+++ T2.sh
@@ -2,11 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-seven_seven="seven is a goodboy"
-sakura="seven is a goodboy"
-l="祝他幸福"
-echo ${seven_seven}
-if [ $seven_seven="seven is a good boy" ]
-then
-    echo ${l}
-fi
+seven_seven="77 is a good girl"
+sakura="sakura too!"
+l=${seven_seven}""${sakura}
+echo ${l}
